13e6bd62035860ef6faf898597ab61f83b6f3112
petruchito <freeglider@gmail.com>
2021-09-12 23:19:36 +0300

    Switch to keyCodes so hotkeys work in other keyboard layouts

--- YTskip.user.js
+++ YTskip.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YTskip
 // @namespace    https://github.com/petruchito/YTskip
-// @version      0.2
+// @version      0.3
 // @description  Ctrl-,. to skip 1 sec
 // @author       petruchito <freeglider@gmail.com>
 // @match        https://www.youtube.com/*
@@ -23,9 +23,9 @@ function onNavigate() {
     } catch (e) {}
 
     onKeyDown = function(e) {
-      if (e.key === ',' && e.ctrlKey) {
+      if (e.code === 'Comma' && e.ctrlKey) {
         V_YOUTUBE_PLAYER.seekBy(-V_SECONDS);
-      } else if (e.key === '.' && e.ctrlKey) {
+      } else if (e.code === 'Period' && e.ctrlKey) {
         V_YOUTUBE_PLAYER.seekBy(V_SECONDS);
       }
     }
